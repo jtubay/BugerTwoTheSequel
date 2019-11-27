377d4500036465a84e1b6d5267f45c2484e03afe
Jean Tubay <jean.tubay.92@gmail.com>
2019-11-27 17:33:49 -0500

    added basic set up

---
+++ db/seed.sql
@@ -0,0 +1,10 @@
+SELECT *FROM burgers;
+
+
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Cheese Burger", false);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Baconnador", false);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Plain Burger", false);
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const mysql = require("mysql");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "rootroot",
+    database: "burgers_db"
+});
+
+connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+app.listen(PORT, () => {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
